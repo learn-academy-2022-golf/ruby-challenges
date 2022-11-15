3a6efb984cfc28bfdca4895f0c8fb988498968df
Shadow090509 <jamoso090509@gmail.com>
2022-11-15 15:58:44 -0800

    push

--- ruby-ciani-ramirez.rb
+++ ruby-ciani-ramirez.rb
@@ -28,10 +28,28 @@ my_name = Ramirez
 3.0.0 :017 > my_name.index("m")
  => 2
 # .swapcase
+3.0.0 :002 > my_name.swapcase
+ => "rAMIREZ"
 # Create an array that contains the name of at least five TV shows you enjoy.
+3.0.0 :003 > my_array = ['game of thrones', 'friends', 'big bang theory', 'how i met your
+ mother', 'scrubs']
+ => ["game of thrones", "friends", "big bang theory", "how i met your mother", "scrubs']
+
 # Find the length of the array.
+3.0.0 :004 > my_array.length
+ => 5 
 # Return the first item in the array.
+3.0.0 :005 > my_array[0]
+ => "game of thrones" 
 # Return the fourth item in the array.
+3.0.0 :006 > my_array[3]
+ => "how i met your mother"
 # Permanently reverse the order of the array.
+3.0.0 :009 > my_array.reverse!
+ => ["scrubs", "how i met your mother", "big bang theory", "friends", "game of thrones"] 
+3.0.0 :010 > my_array
+ => ["scrubs", "how i met your mother", "big bang theory", "friends", "game of thrones"]
 # Create a new empty array for your top favorite TV shows.
+3.0.0 :011 > my_new_array = []
+ => []
 # Using the full TV show array, add your top two favorite shows to the empty array.
