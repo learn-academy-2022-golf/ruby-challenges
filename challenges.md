808210c5ad896e0c75a4caee75e9e6c64204c9d4
Shadow090509 <jamoso090509@gmail.com>
2022-11-15 15:35:12 -0800

    push

--- challenges.md
+++ challenges.md
@@ -87,19 +87,3 @@ Reassign the value of one variable to be 26 times its current value.
 3.0.0 :008 > new_number = 13 * 26
  => 338 
 
- Create a variable and return it in a sentence using string interpolation.
-Create a variable that contains a string and test some of the Ruby string methods:
-.upcase
-.reverse
-.include?
-.capitalize
-.delete
-.index
-.swapcase
-Create an array that contains the name of at least five TV shows you enjoy.
-Find the length of the array.
-Return the first item in the array.
-Return the fourth item in the array.
-Permanently reverse the order of the array.
-Create a new empty array for your top favorite TV shows.
-Using the full TV show array, add your top two favorite shows to the empty array.
---
+++ ruby-ciani-ramirez.rb
@@ -0,0 +1,37 @@
+# Create a variable and return it in a sentence using string interpolation.
+
+my_name = Ramirez
+"hello, my name is #{my_name}"
+3.0.0 :005 > "hello, my name is #{my_name}"
+ => "hello, my name is Ramirez" 
+3.0.0 :006 > 
+                 
+# Create a variable that contains a string and test some of the Ruby string methods:
+# .upcase
+3.0.0 :008 > my_name.upcase
+ => "RAMIREZ" 
+3.0.0 :009 > 
+# .reverse
+3.0.0 :007 > my_name.reverse
+ => "zerimaR" 
+3.0.0 :008 > 
+# .include?
+3.0.0 :012 > my_name.include? 'r'
+ => true 
+# .capitalize
+3.0.0 :013 > my_name.capitalize
+ => "Ramirez" 
+# .delete
+3.0.0 :014 > my_name.delete 'a'
+ => "Rmirez" 
+# .index
+3.0.0 :017 > my_name.index("m")
+ => 2
+# .swapcase
+# Create an array that contains the name of at least five TV shows you enjoy.
+# Find the length of the array.
+# Return the first item in the array.
+# Return the fourth item in the array.
+# Permanently reverse the order of the array.
+# Create a new empty array for your top favorite TV shows.
+# Using the full TV show array, add your top two favorite shows to the empty array.
