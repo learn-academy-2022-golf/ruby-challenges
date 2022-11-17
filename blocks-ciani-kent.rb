62f06a51e44e331dd9bbdfd225505441d965bc4b
cibrown94 <cianibrown94@aol.com>
2022-11-16 16:42:58 -0800

    Block push

---
+++ blocks-ciani-kent.rb
@@ -0,0 +1,94 @@
+# Create a loop that prints the numbers 1 to 20.
+
+# range_nums = 1..20
+# range_nums.each do |numbers|
+#     p numbers
+# end 
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+
+# Create a loop that prints the numbers 1 to 20 using a different approach than previously used.
+
+# range_nums = 1..20
+# range_nums.each do |value|
+#     p value
+# end 
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+
+# Create a loop that prints only even numbers from 20 to 0.
+
+# def only_even num
+#     num.select do |number|
+#         number % 2 == 0
+#     end 
+# end 
+
+# p only_even (0..20)
+
+# [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+
+# Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+
+# new_number_array = [27, 5, 16, 4, 2]
+# new_number_array.each do |value|
+#   p  value * 5
+# end 
+
+# 135
+# 25
+# 80
+# 20
+# 10
+
+# Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+
+new_words = ['story', 'telling' 'great', 'twilight']
+new
+
+
+# Create a method that takes in an array of numbers and returns the largest number in the array.
+# Create a method that takes in an array of words and returns the word with the least number of characters.
+# Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+# Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercase letters should be returned lowercased and all lowercased letters should be returned in uppercase letters.
+# Create a method that will take in an array of numbers and return an array with the remainder of each number when divided by 2.
+# Create a method that will take in a range of letters and return an array with all the letters capitalized.
