65252d40ec75945610c8a88f85e63c89a53bf8a4
tabithatrubey <tabithatrubey@outlook.com>
2022-11-16 16:40:10 -0800

    Great work

---
+++ blocks-philip-tabi.rb
@@ -0,0 +1,32 @@
+# Create a loop that prints the numbers 1 to 20.
+    # range_nums = 1..20
+    # range_nums.each do |value|
+    #     p value
+    # end
+# Create a loop that prints the numbers 1 to 20 using a different approach than previously used.
+    # num = 1
+    # while num <= 20
+    #     p num
+    #     num +=1
+    # end
+# Create a loop that prints only even numbers from 20 to 0.
+    
+
+    range_nums = 0..20
+    
+    def show_evens (nums)
+        nums.select do |num|
+            value.even?
+                "This is an even number"
+        end
+    end
+p show_evens 0..20
+
+# Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+# Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+# Create a method that takes in an array of numbers and returns the largest number in the array.
+# Create a method that takes in an array of words and returns the word with the least number of characters.
+# Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+# Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercase letters should be returned lowercased and all lowercased letters should be returned in uppercase letters.
+# Create a method that will take in an array of numbers and return an array with the remainder of each number when divided by 2.
+# Create a method that will take in a range of letters and return an array with all the letters capitalized.
