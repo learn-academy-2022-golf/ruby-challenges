3ea18ce001da5c9a633c5a406cdcd55d29d80cf8
1611developer <1611.developer@gmail.com>
2022-11-21 12:04:24 -0800

    almost finished

--- classes-philip-tyler.rb
+++ classes-philip-tyler.rb
@@ -10,26 +10,68 @@
     #    washing_bottles = Task.new
     #    takout_trash = Task.new
 # As a developer, I can initialize each instance of class Task with a title.
-    class Task
+    # class Task
 
-        def initialize(chore)
-            @chore = chore
-        end
+    #     def initialize(chore)
+    #         @chore = chore
+    #         @status = 'incomplete'
+    #     end
 
-    end   
+    #     def set_status(status)
+    #         @status = status
+    #     end
+
+    # end   
        
-       laundry = Task.new('laundry')
-       washing_bottles = Task.new('washing bottles')
-       takout_trash = Task.new('taking out the papers and the trash')
+    #    laundry = Task.new('laundry')
+    #    washing_bottles = Task.new('washing bottles')
+    #    takout_trash = Task.new('taking out the papers and the trash')
 # As a developer, I can see the title of each instance of class Task.
-    p laundry
-    p washing_bottles
-    p takout_trash
+    # p laundry
+    # p washing_bottles
+    # p takout_trash
+
+    # laundry.set_status('complete')
+    # washing_bottles.set_status('complete')
+    # takout_trash.set_status('complete')
+    # p laundry
+    # p washing_bottles
+    # p takout_trash
+
+
 
 # As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
+# see line 17
 
 # As a developer, I can update the status of each instance of class Task when the task has been completed.
+# see line 24
+
 # For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
+
+class ColorPalette
+    attr_accessor :title, :color
+
+    def initialize(title, color)
+        @title = title
+        @color = color
+    end
+
+    def all_colors
+        "The colors of #{ti} are "
+    end
+end
+
+green = ColorPalette.new('green')
+blue = ColorPalette.new('blue')
+red = ColorPalette.new('red')
+
+green.color = 'Chartreuse', 'Kelly', 'Seafoam'
+blue.color = 'baby blue', 'sky blue', 'dark blue'
+red.color = 'fire red', '100 miles per hour', 'dark red'
+
+p green.color[0]
+p blue.color[2]
+p red.color[1]
 
 # As a developer, I can create a class called ColorPalette.
 # As a developer, I can create three instances (objects) of class ColorPalette.
